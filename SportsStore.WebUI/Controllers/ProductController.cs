40fec80e1c1a4ef86cb5cf442a4d07624e43362a
ccl <ccl@DESKTOP-BS7UPRD>
2019-07-11 00:29:18 +0800

    添加项目文件。

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository reopository;
+
+        public int PageSize = 4;
+
+        public ProductController(IProductsRepository productsRepository) {
+            this.reopository = productsRepository;
+        }
+
+        public ActionResult List(int page =1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = reopository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = reopository.Products.Count()
+                }
+            };
+            
+            return View(model);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure {
+
+    public class NinjectDependencyResolver : IDependencyResolver {
+        /// <summary>
+        /// 
+        /// </summary>
+        private IKernel kernel;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="kernelParam"></param>
+        public NinjectDependencyResolver(IKernel kernelParam) {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void AddBindings() {
+            //Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> {
+            //    new Product {Name="Football", Price=25 },
+            //    new Product {Name="Surf board", Price=179 },
+            //    new Product {Name="Running shoes", Price=95 },
+            //});
+            //kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
+        }
+    }
+}
